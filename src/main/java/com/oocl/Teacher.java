55bfe8fedcb43065fab340ddab75cd3358f647e7
qicaisheng <qicaisheng@qq.com>
2020-04-07 16:55:19 +0800

    remove welcomeMessage

--- src/main/java/com/oocl/Teacher.java
+++ src/main/java/com/oocl/Teacher.java
@@ -5,16 +5,11 @@ import java.util.List;
 
 public class Teacher extends Person {
 
-    private String welcomeMessage;
     private ArrayList<String> welcomeMessages = new ArrayList<String>();
 
     @Override
     public String introduce() {
         return String.format("My name is %s. I am %s years old. Teaching for the future of world.", this.getName(), this.getAge());
-    }
-
-    public String getWelcomeMessage() {
-        return welcomeMessage;
     }
 
     public void welcome(Student student, Klass klass) {
